43a9d5ae021086a29e2b873d00da2ec7ce543ca7
Marc Anders <marcsapps1@gmail.com>
2016-10-26 20:32:23 -0500

    load select list from file
    removed load datafile for input from json

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-// Get the <datalist> and <input> elements.
-var dataList = document.getElementById('json-datalist');
-var input = document.getElementById('ajax');
+// Get the <parentElmt> and <placeholder> elements.
+var parentElmt = document.getElementById('PillList');
+var placeholder = document.getElementById('placeholder');
 
 // Create a new XMLHttpRequest.
 var request = new XMLHttpRequest();
@@ -11,28 +11,41 @@ request.onreadystatechange = function(response) {
     if (request.status === 200) {
       // Parse the JSON
       var jsonOptions = JSON.parse(request.responseText);
-  
+
+
+// marc create new select
+var selectList = document.createElement("select");
+selectList.setAttribute("id", "mySelect");
+parentElmt.appendChild(selectList);
+
       // Loop over the JSON array.
-      jsonOptions.forEach(function(item) {
+    jsonOptions.forEach(function(item) {
         // Create a new <option> element.
         var option = document.createElement('option');
         // Set the value using the item in the JSON array.
-        option.value = item;
-        // Add the <option> element to the <datalist>.
-        dataList.appendChild(option);
+        //option.value = item;
+        // Add the <option> element to the <parentElmt>.
+//        parentElmt.appendChild(option);
+//
+/////////////
+// marc makinng options
+        option.setAttribute("value", item);
+        option.text = item;
+        selectList.appendChild(option);
+
       });
-      
-      // Update the placeholder text.
-      input.placeholder = "e.g. datalist";
-    } else {
-      // An error occured :(
-      input.placeholder = "Couldn't load datalist options :(";
-    }
+
+    //   // Update the placeholder text.
+    //   placeholder.placeholder = "e.g. datalist";
+    // } else {
+    //   // An error occured :(
+    //   placeholder.placeholder = "Couldn't load datalist options :(";
+    // }
   }
-};
+}};
 
 // Update the placeholder text.
-input.placeholder = "Loading options...";
+// placeholder.placeholder = "Loading options...";
 
 // Set up and make the request.
 request.open('GET','http://marcsapps.com/json/pills.json', true);
