e9a1cd63127f21b71d75fdb695c5514d29243762
Marc Anders <marcsapps1@gmail.com>
2016-10-27 19:36:38 -0500

    .

--- js/logView.js
+++ js/logView.js
@@ -4,17 +4,13 @@
 ///////////////////////////////////////////////
 
 var a = new Date().toString().split("G")[0];
-document.getElementById("timeP").innerHTML = "Time: " + a
-
-
+document.getElementById("timeP").innerHTML = "Time: " + a;
 // ///////////////////////////////////////////
-
+ 
+  
 // /////////////////////////////////////////////
 //  AJAX REQUEST FOR PILL LIST
-///////////////////////////////////////////////
-
-// Get the <parentElmt> div elements.
-var parentElmt = document.getElementById('logUL');
+////////////////////////////////////////////////
 // Create a new XMLHttpRequest.
 var request = new XMLHttpRequest();
 
@@ -25,38 +21,36 @@ request.onreadystatechange = function(response) {
       // Parse the JSON
   //      document.getElementById('logUL').innerHTML=request.responseText
       var jsonOptions = JSON.parse(request.responseText);
-      var z = jsonOptions[0]
-   //         document.getElementById('logUL').innerHTML=z['Date'];
-      // marc create new select
-      var x = 0;
-      var tdj=[];
-      var htmlj;
-      var resultj;
-      // Loop over the JSON array.
-      jsonOptions.forEach(function(j) {
-      x=x+1;
-         
-      resultj=(htmlj?htmlj:"") + '<tr> <td>' + j["Pill"] + '</td><td>' + j["Date"]+'</td></tr>' 
-       document.getElementById('logTb').appendChild= [resultj];
-
-   
-      });
+      // Loop over the JSON array.  // JQUERY   
+ $.each(jsonOptions, function (index, item) {
+    var eachrow = "<tr>"
+                 + "<td>" + item["Pill"] + "</td>"
+                 + "<td>" + item["Date"] + "</td>"
+                 + "</tr>";
+          $('#LogTb').append.innerHTML(eachrow);
+            //  console.log(fixDate((item["Date"])));
+     $('#test'').innerHTML("test") ;
+});
+      };
      //document.getElementById('tlog').appendChild= [resultj];
       }
 
      // An error occured :(
      else {
-     var selectList = document.createElement("p");
-     selectList.createElement.innerHTML('p')= "error loading Log!";
-     parentElmt.appendChild(selectList);
-       // Create a new <option> element. Set value & text to Item
-      //  var option = document.createElement('p');
-      //  option.setAttribute("value", "Error");
-      //  option.text = "Couldn't Load Log List!";
-      //  selectList.appendChild(option);  //
+    $('logTb').append('Error loading Table'); 
+    
        }
-}};
-//
+};
 // Set up and make the request.
 request.open('GET','http://marcsapps.com/log.json', true);
 request.send();
+
+///////////////////////////
+// function fixDate(d){
+// var dd = new date(d);
+
+
+
+// console.log(dd);
+// return dd;
+// }
